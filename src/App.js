8d5c4f2a98170e5ce905597526c78c1118d946df
Suprabh Jain <official.suprabh@gmail.com>
2019-11-09 12:37:59 +0530

    Added Travis

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> We did it.
+          Edit <code>src/App.js</code> We did it...
         </p>
         <a
           className="App-link"
